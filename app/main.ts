1d22cdc984841984c97d9d4c8e89a1142e038538
ryjacky <ryjacky@outlook.com>
2023-10-09 00:21:26 +0900

    made main file easier to read

--- app/main.ts
+++ app/main.ts
@@ -8,91 +8,83 @@ import {PieEditorWindow} from "./src/pieletteWindows/PieEditorWindow";
 import {PieletteEnv} from "pielette-core/lib/PieletteEnv";
 import {SplashScreenWindow} from "./src/pieletteWindows/SplashScreenWindow";
 
-// Variables
+// ------------------------------- Global Variables -------------------------------
 let pieMenuWindow: PieMenuWindow | undefined;
 let editorWindow: EditorWindow | undefined;
+let splashScreenWindow: SplashScreenWindow | undefined;
 app.setPath("userData", PieletteEnv.DEFAULT_DATA_PATH);
 
 let tray = null;
 
+// ------------------------------- Main -------------------------------
+// Added 400 ms to fix the black background issue while using transparent window.
+// More details at https://github.com/electron/electron/issues/15947
+app.on('ready', () => setTimeout(openSplashScreen, 400));
+
+// Note: windows-all-closed listener is removed
+// because we want to keep the app running in the background
+
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (editorWindow === null) {
+    openSplashScreen();
+  }
+});
+
 // Initialization
 // User data is initialized in app.component.ts and can only be initialized there (with minimal code).
-initElectronWindows();
+
 initElectronAPI();
 initLoggerForRenderer();
-initSystemTray();
-PieletteAddonManager.loadPlugins();
 
-// Functions
+PieletteAddonManager.loadPlugins().then(() => {
+  splashScreenWindow?.close();
 
-function initElectronWindows() {
-  try {
-
-    // Added 400 ms to fix the black background issue while using transparent window.
-    // More details at https://github.com/electron/electron/issues/15947
-    app.on('ready', () => setTimeout(createWindow, 400));
-
-    // Note: windows-all-closed listener is removed
-    // because we want to keep the app running in the background
-
-    app.on('activate', () => {
-      // On OS X it's common to re-create a window in the app when the
-      // dock icon is clicked and there are no other windows open.
-      if (editorWindow === null) {
-        createWindow();
-      }
-    });
-
-  } catch (e) {
-    Log.main.error(e);
-  }
-}
-
-function createWindow(): BrowserWindow {
   pieMenuWindow = new PieMenuWindow();
   editorWindow = new EditorWindow();
-  new SplashScreenWindow();
+
+  initSystemTray();
 
   new PieEditorWindow(1);
+});
 
-  return editorWindow;
+// ------------------------------- Helper Functions -------------------------------
+function openSplashScreen(): BrowserWindow {
+  splashScreenWindow = new SplashScreenWindow();
+  return splashScreenWindow;
 }
 
 function initSystemTray() {
-  app.whenReady().then(() => {
-    // Get screen size
-    const { screen } = require('electron')
-
-    tray = new Tray(__dirname + '/assets/favicon.ico')
-    const contextMenu = Menu.buildFromTemplate([
-      {label: 'AutoHotPie', type: "normal", enabled: false},
-      {type: "separator"},
-      {
-        label: 'Setting', type: "normal", click: () => {
-          editorWindow?.show();
-        }
-      },
-      {
-        label: 'Restart', type: "normal", click: () => {
-          // TODO: IMPLEMENTATION
-          Log.main.warn("Item1 clicked")
-        }
-      },
-      {
-        label: 'Reload Service', type: "normal", click: () => {
-          // TODO: IMPLEMENTATION
-          Log.main.warn("Item1 clicked")
-        }
-      },
-      {
-        label: 'Exit', type: "normal", click: () => {
-          app.exit();
-        }
-      },
-    ])
-    tray.setToolTip('Pie menyuuus!')
-    tray.setContextMenu(contextMenu)
-  })
+  tray = new Tray(__dirname + '/assets/favicon.ico')
+  const contextMenu = Menu.buildFromTemplate([
+    {label: 'AutoHotPie', type: "normal", enabled: false},
+    {type: "separator"},
+    {
+      label: 'Setting', type: "normal", click: () => {
+        editorWindow?.show();
+      }
+    },
+    {
+      label: 'Restart', type: "normal", click: () => {
+        // TODO: IMPLEMENTATION
+        Log.main.warn("Item1 clicked")
+      }
+    },
+    {
+      label: 'Reload Service', type: "normal", click: () => {
+        // TODO: IMPLEMENTATION
+        Log.main.warn("Item1 clicked")
+      }
+    },
+    {
+      label: 'Exit', type: "normal", click: () => {
+        app.exit();
+      }
+    },
+  ])
+  tray.setToolTip('Pie menyuuus!')
+  tray.setContextMenu(contextMenu)
 }
 
 
