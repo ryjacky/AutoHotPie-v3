8d326a32e3c814f969c695f0b6b20a7d1462bd26
ryjacky <ryjacky@outlook.com>
2023-11-07 13:44:48 +0900

    fix: save not working in pie menu editor

--- src/app/core/services/pieMenu/pie-menu.service.ts
+++ src/app/core/services/pieMenu/pie-menu.service.ts
@@ -18,19 +18,50 @@ class PieMenuUnloadedError extends Error {}
 export class PieMenuService extends PieMenu {
   // <PieItemId, PieItem>
   public readonly pieItems = new Map<number, IPieItem | undefined>();
-  public changesSaved = true;
-
   private state = PieMenuServiceState.unloaded;
 
-  constructor(private dbService: DBService) {super();}
+  constructor(
+    private dbService: DBService,
+  ) {
+    super();
+  }
 
-  /**
-   * Get the PieMenu object from the service.
-   */
-  public get pieMenu(): IPieMenu {
-    const pieMenu = new PieMenu();
-    Object.assign(pieMenu, this);
-    return pieMenu;
+  public get basePieMenu(): IPieMenu {
+    const basePieMenu = new PieMenu();
+    for (const pieMenuKey in basePieMenu) {
+      if (pieMenuKey in this) {
+        // @ts-ignore
+        basePieMenu[pieMenuKey] = this[pieMenuKey];
+      }
+    }
+    return basePieMenu;
+  }
+
+  public getPieItemNameAt(pieItemId: number): string {
+    return this.pieItems.get(pieItemId)?.name ?? '';
+  }
+
+  public isIconAtPieItemNative(pieItemId: number): boolean {
+    return (this.pieItems.get(pieItemId ?? -1)?.iconBase64 ?? '').startsWith('[eva]');
+  }
+
+  public getPieItemIconAt(pieItemId: number): string {
+    if (this.isIconAtPieItemNative(pieItemId)){
+      return (this.pieItems.get(pieItemId ?? -1)?.iconBase64 ?? '').replace('[eva]', '');
+    }
+
+    return this.pieItems.get(pieItemId ?? -1)?.iconBase64 ?? '';
+  }
+  public addPieItemTaskContext(pieItemId: number, pieSingleTaskContext: PieSingleTaskContext): void {
+    this.getPieItemTaskContexts(pieItemId).push(pieSingleTaskContext);
+    this.save();
+  }
+  public async addEmptyPieItem() {
+    const pieItem = new PieItem('');
+    pieItem.id = await this.dbService.pieItem.put(pieItem) as number;
+
+    this.pieItems.set(pieItem.id, pieItem);
+    this.pieItemIds.push(pieItem.id);
   }
 
   /**
@@ -42,13 +73,7 @@ export class PieMenuService extends PieMenu {
     return this.load(pieMenuId);
   }
 
-  /**
-   * Load the pie menu from the database.
-   * If the service is already loading a pie menu, the request will be ignored.
-   *
-   * @param pieMenuId The id of the pie menu to load.
-   */
-  public async load(pieMenuId: number) {
+  public async load(pieMenuId: number, reload = false){
     if (this.state === PieMenuServiceState.loading) {
       throw new PieMenuStateError('Pie Menu Service locked, loading in progress');
     }
@@ -57,53 +82,41 @@ export class PieMenuService extends PieMenu {
     }
 
     this.state = PieMenuServiceState.loading;
-    const pieMenu = await this.dbService.pieMenu.get(pieMenuId);
 
+    this.id = pieMenuId;
+    const pieMenu = await this.dbService.pieMenu.get(pieMenuId);
     if (!pieMenu) {
       this.state = PieMenuServiceState.unloaded;
-      throw new PieMenuNotFoundError('Pie Menu not found');
+      throw new PieMenuNotFoundError();
     }
 
-    Object.assign(this, pieMenu);
+    // Not using Object.assign(this, pieMenu); because we cannot guarantee
+    // that the database object has the same properties as the class,
+    // e.g. a structure change due to program update.
+    for (const pieMenuKey in pieMenu) {
+      if (pieMenuKey in this) {
+        // @ts-ignore
+        this[pieMenuKey] = pieMenu[pieMenuKey];
+      }
+    }
 
     this.pieItems.clear();
+    window.log.debug(`${this.pieItems.size} pie items loaded`);
 
     const pieItems = await this.dbService.pieItem.bulkGet(pieMenu.pieItemIds);
     for (let i = 0; i < pieItems.length; i++) {
+      window.log.debug('Loading pie item ' + pieMenu.pieItemIds[i]);
       if (pieItems[i] === undefined) {
-        this.state = PieMenuServiceState.unloaded;
-        throw new PieItemNotFoundError(`Pie Item of id ${pieMenu.pieItemIds[i]} not found`);
+        window.log.warn('Trying to load work area but pie Item of id ' + pieMenu.pieItemIds[i] + ' not found');
+        continue;
       }
+
       this.pieItems.set(pieMenu.pieItemIds[i], pieItems[i]);
     }
 
     window.log.debug(`${this.pieItems.size} pie items loaded`);
+
     this.state = PieMenuServiceState.loaded;
-  }
-
-  /**
-   * Get the displayed name of a pie item.
-   * @param pieItemId
-   */
-  public getPieItemNameAt(pieItemId: number): string {
-    return this.pieItems.get(pieItemId)?.name ?? '';
-  }
-
-  /**
-   * Whether the icon of a pie item is a native Eva icon or an icon in base64.
-   * @param pieItemId
-   */
-  public isIconAtPieItemNative(pieItemId: number): boolean {
-    return (this.pieItems.get(pieItemId ?? -1)?.iconBase64 ?? '').startsWith('[eva]');
-  }
-
-
-  public getPieItemIconAt(pieItemId: number): string {
-    if (this.isIconAtPieItemNative(pieItemId)) {
-      return (this.pieItems.get(pieItemId ?? -1)?.iconBase64 ?? '').replace('[eva]', '');
-    }
-
-    return this.pieItems.get(pieItemId ?? -1)?.iconBase64 ?? '';
   }
 
   public getPieTaskContext(pieItemId: number): PieSingleTaskContext[] | undefined {
@@ -114,28 +127,9 @@ export class PieMenuService extends PieMenu {
     return this.pieItems.get(id) !== undefined;
   }
 
-  public getReadonlyPieItemTaskContexts(id: number): ReadonlyArray<PieSingleTaskContext> {
-    return this.getPieItemTaskContexts(id);
+  public getPieItemTaskContexts(id: number): PieSingleTaskContext[] {
+    return this.pieItems.get(id)?.pieTaskContexts ?? [];
   }
-
-  // --------------------------------------------------------------
-  // Setters
-  // --------------------------------------------------------------
-
-  public addPieItemTaskContext(pieItemId: number, pieSingleTaskContext: PieSingleTaskContext): void {
-    this.getPieItemTaskContexts(pieItemId).push(pieSingleTaskContext);
-    this.save();
-  }
-  public async addEmptyPieItem() {
-    const pieItem = new PieItem('');
-    pieItem.id = await this.dbService.pieItem.put(pieItem) as number;
-
-    this.pieItems.set(pieItem.id, pieItem);
-    this.pieItemIds.push(pieItem.id);
-
-    this.save();
-  }
-
 
   movePieItemUp(i: number) {
     if (i > 0) {
@@ -147,7 +141,6 @@ export class PieMenuService extends PieMenu {
     // Resetting the reference to force the UI to update
     this.pieItemIds = [...this.pieItemIds];
     this.dbService.pieMenu.update(this.id ?? -1, {pieItemIds: this.pieItemIds});
-    this.save();
   }
 
   movePieItemDown(i: number) {
@@ -160,7 +153,6 @@ export class PieMenuService extends PieMenu {
     // Resetting the reference to force the UI to update
     this.pieItemIds = [...this.pieItemIds];
     this.dbService.pieMenu.update(this.id ?? -1, {pieItemIds: this.pieItemIds});
-    this.save();
   }
 
   public setPieItemActions(id: number, actions: PieSingleTaskContext[]) {
@@ -171,9 +163,7 @@ export class PieMenuService extends PieMenu {
     // this.pieItems.get(id)?.actions must not be undefined
     // eslint-disable-next-line  @typescript-eslint/no-non-null-assertion
     this.pieItems.get(id)!.pieTaskContexts = actions;
-    this.save();
   }
-
   public movePieTaskUp(pieItemId: number, pieTaskIndex: number) {
     const actions = this.getPieItemTaskContexts(pieItemId);
     if (pieTaskIndex > 0) {
@@ -207,63 +197,14 @@ export class PieMenuService extends PieMenu {
     }
     this.save();
   }
-
-  setEscapeRadius(value: number): void {
-    this.escapeRadius = value;
-    this.save();
-  }
-
-  setIconSize(value: number): void {
-    this.iconSize = value;
-    this.save();
-  }
-
-  setCenterRadius(value: number): void {
-    this.centerRadius = value;
-    this.save();
-  }
-
-  setCenterThickness(value: number): void {
-    this.centerThickness = value;
-    this.save();
-  }
-
-  setPieItemSpread(value: number): void {
-    this.pieItemSpread = value;
-    this.save();
-  }
-
-  setPieItemRoundness(value: number): void {
-    this.pieItemRoundness = value;
-    this.save();
-  }
-
-  setPieItemWidth(value: number): void {
-    this.pieItemWidth = value;
-    this.save();
-  }
-
-  setMainColor(value: string): void {
-    this.mainColor = value;
-    this.save();
-  }
-
-  setSecondaryColor(value: string): void {
-    this.secondaryColor = value;
-    this.save();
-  }
-
-  setIconColor(value: string): void {
-    this.iconColor = value;
-    this.save();
-  }
-
   public async save() {
-    if (this.state === PieMenuServiceState.unloaded) { throw new PieMenuUnloadedError(); }
+    if (this.state !== PieMenuServiceState.loaded) {
+      window.log.warn('Cannot save pie menu state, not loaded');
+      return;
+    }
 
-    this.changesSaved = false;
-
-    await this.dbService.pieMenu.update(this.id ?? -1, this.pieMenu);
+    window.log.debug('Saving pie menu state: ' + JSON.stringify(this.basePieMenu));
+    await this.dbService.pieMenu.update(this.id ?? -1, this.basePieMenu);
 
     const nonEmptyPieItems: PieItem[] = [];
     for (const pieItem of this.pieItems.values()) {
@@ -273,13 +214,11 @@ export class PieMenuService extends PieMenu {
     }
     await this.dbService.pieItem.bulkPut(nonEmptyPieItems);
 
-    this.changesSaved = true;
   }
 
   removePieItem(pieItemId: number) {
     this.pieItems.delete(pieItemId);
     this.pieItemIds = this.pieItemIds.filter(id => id !== pieItemId);
-    this.save();
   }
 
   removeIconAtPieItem(pieItemId: number) {
@@ -288,10 +227,5 @@ export class PieMenuService extends PieMenu {
     if (pieItem !== undefined) {
       pieItem.iconBase64 = '';
     }
-    this.save();
-  }
-
-  private getPieItemTaskContexts(id: number): PieSingleTaskContext[] {
-    return this.pieItems.get(id)?.pieTaskContexts ?? [];
   }
 }
